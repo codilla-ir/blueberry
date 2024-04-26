a8a84db0e6d521671da4db8394531482c89e57af
Mohammad <m.chahkandi2@gmail.com>
2024-04-26 16:38:34 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "brown": {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        }
+      },
+      boxShadow: {
+        "shadow-normal": "0px 1px 10px rgba(0, 0, 0, 0.05)"
+      },
+      borderRadius: {
+        "4xl": "2rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
