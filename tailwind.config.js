eaa1340473e646eea75cb4fad79fde14fa7ff115
Mohammad <m.chahkandi2@gmail.com>
2024-04-29 22:51:05 +0330

    feta: header submenu

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,23 @@ module.exports = {
         }
       },
       boxShadow: {
-        "shadow-normal": "0px 1px 10px rgba(0, 0, 0, 0.05)"
+        "normal": "0px 1px 10px rgba(0, 0, 0, 0.05)"
       },
       borderRadius: {
         "4xl": "2rem"
-      }
+      },
+      fontFamily: {
+        "vazir": "",
+        "vazir": "",
+        "vazir": "",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    }
+  ],
 }
 
